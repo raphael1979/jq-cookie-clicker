5ce6c4ff38a87a5202fea4ad28950a515db2e155
raphael <moudens.raphael@orange.fr>
2017-01-06 16:30:19 +0100

    fonctionnel

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 	//votre code
+		var phrases = ["Poussé la carcasse à bord, mais tous les vices.", "Reprends les clefs, après le portefeuille de son mari dans le quartier même où l'air ne fonctionnait pas.", "Celle-là n'est qu'ils ont du cachalot et la lanterne commença de descendre.", "Agité par ces pensées, qui appellent ces oeuvres à la foi de ces paysans l'imprégner de son effluve."]; 
 
+	var i = $('.counter').text();
+	$('.cake').click(function(){
+		i++
+		$('.counter').html(i);
+		if((i)%20 == 0){
+			alert(phrases[Math.floor(Math.random()*phrases.length)]);
+		}
+		console.log(i);
+	})
 
 });
+
